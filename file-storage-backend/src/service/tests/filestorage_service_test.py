b326bf1f7e66dbf2e94561b6ad725da48f3b725b
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-02-28 00:41:27 +0900

    Move tests to another location

--- file-storage-backend/src/service/tests/filestorage_service_test.py
+++ file-storage-backend/src/service/tests/filestorage_service_test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from ..filestorage_service import FileStorageService
+
 def test_get_files():
     service = FileStorageService(RedisFactoryMock())
     assert service.get_files() == [{ 'filename': 'test', 'size': 112 }]
@@ -8,14 +10,11 @@ def test_get_files():
 class RedisFactoryMock:
     def create_instance(self):
         print('test')
-        return 
+        return RedisMock()
 
 class RedisMock:
     def mget(self, keys):
         assert keys == ['K1', 'K2']
-        return [{
-            'requested_filename': 'test',
-            'size': 112
-        }]
+        return ['{"requested_filename": "test", "size": 112}']
     def keys(self, pattern):
         return ['K1', 'K2']
